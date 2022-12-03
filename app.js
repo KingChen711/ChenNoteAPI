3da6133d9e988049c017a44a703b5dcf721afcc4
KingChen711 <Kingchenobama711@gmail.com>
2022-12-03 22:47:36 +0700

    CORS4

--- app.js
+++ app.js
@@ -25,15 +25,25 @@ const connectDB = async () => {
 connectDB()
 
 
-
-app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", 'https://king-chen-note.netlify.app');
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST ,DELETE, OPTIONS');
-  res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://king-chen-note.netlify.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
-})
+});
+app.use(express.json())
 app.use("/api/auth", authRouter)
 app.use("/api/notes", notesRouter)
 
