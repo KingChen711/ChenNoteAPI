ad2f8b3227650fe838985e08e85ff4e9ff5e9c35
KingChen711 <Kingchenobama711@gmail.com>
2022-12-03 22:37:44 +0700

    CORS2

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./routes/auth.js');
 const notesRouter = require('./routes/notes.js');
-var cors = require('cors');
 
 const app = express();
 
@@ -27,13 +26,12 @@ connectDB()
 
 
 
-app.use(cors())
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.setHeader("Access-Control-Allow-Origin", 'https://king-chen-note.netlify.app/');
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST ,DELETE, OPTIONS');
+  res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 })
 app.use("/api/auth", authRouter)
