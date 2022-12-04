5f257d00f78729f3e8130c6bc3a834b090ea4714
KingChen711 <Kingchenobama711@gmail.com>
2022-12-05 02:33:15 +0700

    CORS,5/12

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./routes/auth.js');
 const notesRouter = require('./routes/notes.js');
-
+var cors = require('cors')
 const app = express();
 
 
@@ -25,24 +25,9 @@ const connectDB = async () => {
 connectDB()
 
 
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://king-chen-note.netlify.app');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRouter)
 app.use("/api/notes", notesRouter)
@@ -50,7 +35,7 @@ app.use("/api/notes", notesRouter)
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 })
